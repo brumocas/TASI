258240f93cdb05afbb481b671ef8da9e21d2a5be
brumocas <brucosta2002@gmail.com>
2024-11-10 18:27:28 +0000

    Hug Course update

---
+++ Hugging_Face_Course/Unit7/push.sh
@@ -0,0 +1,5 @@
+# Ask for the tokken
+#huggingface-cli login
+
+# Push to the hub
+mlagents-push-to-hf  --run-id="SoccerTwos" --local-dir="./results/SoccerTwos" --repo-id="Brumocas/poca-SoccerTwos" --commit-message="First Push"
---
+++ Hugging_Face_Course/Unit7/train.sh
@@ -0,0 +1,3 @@
+mlagents-learn ./config/poca/SoccerTwos.yaml --env=./training-envs-executables/linux/SoccerTwos/SoccerTwos.x86_64 --run-id="SoccerTwos" --no-graphics --resume
+
+
